648e82e4192dd4ffb843d279ba08ca1ca3036584
Eliott Nys <eliott.nys@gmail.com>
2023-12-18 10:07:28 +0100

    added deletion of images in reset_db.py

--- reset_db.py
+++ reset_db.py
@@ -1,3 +1,19 @@
 import json
+import os
 with open("db.json", 'w') as json_file:
     json.dump({"images": {},"faces": {},"tags": {"humans": {},"animals": {},"custom_tags": {}}}, json_file)
+
+def delete_files_in_directory(directory_path):
+    for file_name in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, file_name)
+        
+        if os.path.isfile(file_path):
+            try:
+                os.remove(file_path)
+                print(f"Deleted: {file_path}")
+            except Exception as e:
+                print(f"Error deleting {file_path}: {e}")
+
+directories = ["static/faces", "static/tmp_image", "static/tmp_upload", "static/uploads"]
+for directory in directories:
+    delete_files_in_directory(directory)
